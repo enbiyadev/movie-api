32b0588ddb07ccc1f3b56129b6e58d33f81bc45d
Enbiya <enbiyacandogan@outlook.com>
2018-11-11 18:36:23 +0300

    test'ler güncellendi ve travis

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -5,13 +5,13 @@ const server = require('../../app');
 
 chai.use(chaiHttp);
 
-describe("Node Server", () => {
-  it('(GET /) anasayfayı döndürür', (done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-      });
-  });
+describe('Node Server', () => {
+	it('(GET /) returns the homepage', (done) => {
+		chai.request(server)
+			.get('/')
+			.end((err, res) => {
+				res.should.have.status(200);
+				done();
+			})
+	});
 });
--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -2,89 +2,86 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const server = require('../../app');
-
 chai.use(chaiHttp);
 
 let token, movieId;
 
-describe("/api/movies Testler", () => {
-  
-  before((done) => {
-    chai.request(server)
-      .post('/authenticate')
-      .send({ username: "enbiyacan55", password: "11111" })
-      .end((err, res) => {
-        token = res.body.token;
-        done();
-      });
-  });
+describe('/api/movies tests', () => {
+	before((done) => {
+		chai.request(server)
+			.post('/authenticate')
+			.send({username: 'mseven', password: '12345'})
+			.end((err, res) => {
+				token = res.body.token;
+				done();
+			});
+	});
 
-  describe("/GET Filmler", () => {
-    it("Tüm filmleri döndürürmeli", (done) => {
-      chai.request(server)
-        .get('/api/movies')
-        .set('x-access-token', token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          done();
-        });
-    });
-  });
+	describe('/GET movies', () => {
+		it('it should GET all the movies', (done) => {
+			chai.request(server)
+				.get('/api/movies')
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
+		})
+	});
 
-  describe("/POST Film", () => {
-    it("Bir film getirmeli", (done) => {
-      const movie = {
-        title: "Test",
-        director_id: "5bc9814ce1d4ac432caa998a",
-        category: "Komedi",
-        country: "Türkiye",
-        year: 1950,
-        imdb_score: 8
-      }
+	describe('/POST movie', () => {
+		it('it should POST a movie', (done) => {
+			const movie = {
+				title: 'Udemy',
+				director_id: '5a34e1afb8523a78631f8540',
+				category: 'Komedi',
+				country: 'Türkiye',
+				year: 1950,
+				imdb_score: 8
+			};
 
-      chai.request(server)
-        .post('/api/movies')
-        .send(movie)
-        .set("x-access-token", token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("title");
-          res.body.should.have.property("director_id");
-          res.body.should.have.property("category");
-          res.body.should.have.property("country");
-          res.body.should.have.property("year");
-          res.body.should.have.property("imdb_score");
-          movieId = res.body._id;
-          done();
-        });
-    });
+			chai.request(server)
+				.post('/api/movies')
+				.send(movie)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+					movieId = res.body._id;
+					done();
+				});
+		});
+	});
 
-  });
-  
-  describe("/GET/:director_id Film", () => {
-    it("Eşleşen id ile filmi getirmeli", (done) => {
-      chai.request(server)
-        .get("/api/movies/" + movieId )
-        .set("x-access-token", token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("title");
-          res.body.should.have.property("director_id");
-          res.body.should.have.property("category");
-          res.body.should.have.property("country");
-          res.body.should.have.property("year");
-          res.body.should.have.property("imdb_score");
-          res.body.should.have.property("_id").eql(movieId);
-          done();
-        });
-    });
-  });
+	describe('/GET/:movie_id movie', () => {
+		it('it should GET a movie by the given id', (done) => {
+			chai.request(server)
+				.get('/api/movies/' + movieId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+					res.body.should.have.property('_id').eql(movieId);
+					done();
+				});
+		});
+	});
 
-  describe('/PUT/:movie_id Film', () => {
-		it('Bir film güncellenmeli', (done) => {
+	describe('/PUT/:movie_id movie', () => {
+		it('it should UPDATE a movie given by id', (done) => {
 			const movie = {
 				title: '93creative',
 				director_id: '5a34e1afb8523a78631f8541',
@@ -111,20 +108,19 @@ describe("/api/movies Testler", () => {
 					done();
 				});
 		});
-  });
+	});
 
-  describe('/DELETE/:movie_id Film', () => {
-		it('Bir film silinmeli', (done) => {
+	describe('/DELETE/:movie_id movie', () => {
+		it('it should DELETE a movie given by id', (done) => {
 			chai.request(server)
 				.delete('/api/movies/' + movieId)
 				.set('x-access-token', token)
 				.end((err, res) => {
 					res.should.have.status(200);
 					res.body.should.be.a('object');
-					// res.body.should.have.property('status').eql(1);
+					res.body.should.have.property('status').eql(1);
 					done();
 				});
 		});
-  });
-
+	});
 });
